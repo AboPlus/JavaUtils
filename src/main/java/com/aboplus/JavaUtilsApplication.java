68ace214c78483a59e9f6d188c2e7e006ab7c832
WangJiabao <252841200@qq.com>
2022-01-01 17:45:36 +0800

    Initial commit

---
+++ src/main/java/com/aboplus/JavaUtilsApplication.java
@@ -0,0 +1,15 @@
+package com.aboplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Abo
+ * @date : 2022/1/1 17:39
+ */
+@SpringBootApplication
+public class JavaUtilsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilsApplication.class, args);
+    }
+}
